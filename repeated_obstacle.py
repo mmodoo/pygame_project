6882556565a225bb2b24f57a49c49e416edb2145
mmodoo <at2314@naver.com>
2023-08-13 15:53:22 +0900

    second commit

--- repeated_obstacle.py
+++ repeated_obstacle.py
@@ -1,3 +1,4 @@
+""" cave - Copyright 2016 Kenichiro Tanaka  """
 # -*- coding: utf-8 -*-
 """
 Created on Thu May  4 17:44:13 2023
@@ -15,8 +16,9 @@ pygame.key.set_repeat(5, 5)
 SURFACE = pygame.display.set_mode((800, 600))
 FPSCLOCK = pygame.time.Clock()
 
+
 def main():
-    """ 메인 루틴 """
+    """메인 루틴"""
     walls = 80
     ship_y = 250
     velocity = 0
@@ -28,13 +30,12 @@ def main():
     bang_image = pygame.image.load("bang.png")
     obstacle_image = pygame.image.load("obstacle.png")
     obstacle = obstacle_image.get_rect()
-    obstacle.center = (700,400)
-              
+    obstacle.center = (700, 400)
 
     holes = []
     for xpos in range(walls):
         holes.append(Rect(xpos * 10, 100, 10, 400))
-        
+
     game_over = False
 
     while True:
@@ -66,59 +67,61 @@ def main():
             holes.append(edge)
             del holes[0]
             holes = [x.move(-10, 0) for x in holes]
-                        
 
             # 충돌?
-            if holes[0].top > ship_y or \
-                holes[0].bottom < ship_y + 80:
+            if holes[0].top > ship_y or holes[0].bottom < ship_y + 80:
                 game_over = True
-                
-            if ship_y+40 > obstacle.top and ship.right > obstacle.left \
-                and ship_y-40 < obstacle.bottom :
+
+            if (
+                ship_y + 40 > obstacle.top
+                and ship.right > obstacle.left
+                and ship_y - 40 < obstacle.bottom
+            ):
                 game_over = True
-                
-             
+
         # 그리기
         SURFACE.fill((0, 125, 125))
 
         for hole in holes:
             pygame.draw.rect(SURFACE, (0, 0, 0), hole)
         SURFACE.blit(ship_image, (0, ship_y))
-        score_image = sysfont.render("score is {}".format(score), 
-                                     True, (0, 0, 225))
+        score_image = sysfont.render("score is {}".format(score), True, (0, 0, 225))
         SURFACE.blit(score_image, (600, 20))
 
-        #장애물 생성 코드
+        # 장애물 생성 코드
         if not game_over:
             obstacle.move_ip(-13, 0)
-        
-            pygame.draw.rect(SURFACE, (255,255,255), obstacle)
-            
-        #그림과 장애물 합치기
+
+            pygame.draw.rect(SURFACE, (255, 255, 255), obstacle)
+
+            # 그림과 장애물 합치기
             obstacle2_center = list(obstacle.center)
-            [x,y] = obstacle2_center     
-            obstacle_pos = Rect(x-26,y-25,26,25)
-        
+            [x, y] = obstacle2_center
+            obstacle_pos = Rect(x - 26, y - 25, 26, 25)
+
             SURFACE.blit(obstacle_image, obstacle_pos)
-        
-            print(obstacle.left,obstacle.top,ship_y, ship.right, )
-        
+
+            print(
+                obstacle.left,
+                obstacle.top,
+                ship_y,
+                ship.right,
+            )
+
             if obstacle.right < 50:
                 slope2 = randint(-3, 3)
-                obstacle.center = (700, 300 + slope2*50) 
-                
-                    
+                obstacle.center = (700, 300 + slope2 * 50)
+
         if game_over:
-            SURFACE.blit(bang_image, (0, ship_y-40))
-            restart_image = sysfont.render("Restart : Press R key", 
-                                     True, (255, 0, 0))
+            SURFACE.blit(bang_image, (0, ship_y - 40))
+            restart_image = sysfont.render("Restart : Press R key", True, (255, 0, 0))
             SURFACE.blit(restart_image, (280, 300))
             if is_rkey_down:
                 main()
-            
 
         pygame.display.update()
         FPSCLOCK.tick(20)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
